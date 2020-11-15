2f8938ec3159711e333a2917cb665975f9c3589b
Vladislavs Pozņaks <vladislavs.poznaks@gmail.com>
2020-11-15 16:24:57 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import {connect} from "react-redux";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// @ts-ignore
+const App = (state) => {
+    console.log(state);
+    return (
+        <div className="App">
+            <h1 className="bg-blue-400">Micro Lending</h1>
+        </div>
+    );
 }
 
-export default App;
+// @ts-ignore
+const mapStateToProps = (state) => {
+    return state;
+}
+
+export default connect(mapStateToProps)(App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import './styles/main.css';
+
+const initial = {
+    test: 'test',
+    loans: []
+}
+
+const store = createStore((state = initial, action) => {
+    return state;
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+        <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
